a676f117285c06616e414a3d8ef63b8b297e8b07
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-10-13 17:28:29 +0100

    config db
    preparando o ambiente do banco de dados

---
+++ app/models/table.py
@@ -0,0 +1,16 @@
+from app import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    name = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
+
+    def __init__(self, username, password, name, email):
+        self.username = username
+        self.password = password
+        self.name = name
+        self.email = email
